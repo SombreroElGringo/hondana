b655a4007448a49ce1ad371c1e554c0c96118fd2
SombreroElGringo <florent.pailhes@hotmail.fr>
2018-11-15 15:13:22 +0100

    feature(SN-14) Add Swagger
    • Implement Swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { Transport } from "@nestjs/microservices";
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ApplicationModule } from "./app.module";
 
 async function bootstrap() {
@@ -8,6 +9,15 @@ async function bootstrap() {
     transport: Transport.TCP,
     options: { retryAttempts: 5, retryDelay: 3000 },
   });
+
+
+  const options = new DocumentBuilder()
+    .setTitle('SNCF-Live API')
+    .setDescription('The SNCF-Live API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/v1/docs', app, document);
 
   await app.startAllMicroservicesAsync();
   await app.listen(3001);
