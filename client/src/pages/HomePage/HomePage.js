89384e52129943b5625c4c8346c7ea308733049f
Dylan Chamoulaud <dylan3396@hotmail.fr>
2018-11-16 17:39:15 +0100

    feature(SN-18): Getting started Homepage
    Starting the homepage design

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,9 +1,62 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+import "./HomePage.css"
 
-class HomePage extends Component{
-    render(){
-        return <h1>HomePage</h1>
-    }
+class HomePage extends Component {
+  state = {
+    stations: undefined,
+  };
+
+  submitHandler() {
+	  this.setState({
+	    stations: [
+        { name: 'Bordeaux saint-jean' },
+        { name: 'Paris (Toutes gares)' },
+        { name: 'Arcachon' },
+      ],
+    });
+  }
+
+  componentDidMount(){
+  	this.submitHandler();
+  }
+
+  render() {
+    const { stations } = this.state;
+
+    return (
+      <div className={"container"} id={"homepage"}>
+        <div className={'search-station'}>
+          <form
+            onSubmit={e => {
+              e.preventDefault();
+              this.submitHandler();
+            }}
+          >
+            <div className="d-flex">
+              <input
+                className="form-control"
+                type="text"
+                placeholder={'Renseignez votre gare'}
+              />
+              <button className={'btn btn-info'} type={'submit'}>
+                Rechercher
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="stations-result mt-4">
+          <div className="row">
+            {stations &&
+              stations.map(station => {
+                return <div className={"col-md-4"}>
+	                <b>{station.name}</b>
+                </div>;
+              })}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default HomePage;
