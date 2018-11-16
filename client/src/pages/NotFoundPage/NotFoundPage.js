8ab4f461f6122f5ac1c622c13048c31256d2344e
Dylan Chamoulaud <dylan3396@hotmail.fr>
2018-11-16 14:06:55 +0100

    feature(SN-16): Bootstrap
    Add bootstrap to the front-end project

--- client/src/pages/NotFoundPage/NotFoundPage.js
+++ client/src/pages/NotFoundPage/NotFoundPage.js
@@ -1,11 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class NotFoundPage extends Component{
-    render(){
-        return(
-            <h1>NorFoundPage</h1>
-        )
-    }
+class NotFoundPage extends Component {
+  render() {
+    return (
+      <div className={'container text-center'}>
+        <img className={'img-fluid'} src={'https://http.cat/404'} />
+      </div>
+    );
+  }
 }
 
 export default NotFoundPage;
