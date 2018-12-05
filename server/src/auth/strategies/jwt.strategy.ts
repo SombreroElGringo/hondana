db11bbbed1b1d14abbf971ca0e29de234a58bff6
SombreroElGringo <florent.pailhes@hotmail.fr>
2018-12-05 22:53:45 +0100

    Auth dead code

--- server/src/auth/strategies/jwt.strategy.ts
+++ server/src/auth/strategies/jwt.strategy.ts
@@ -15,9 +15,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   async validate(payload: JwtPayload) {
     const user = await this.authService.validateUser(payload);
-    if (!user) {
-      throw new UnauthorizedException();
-    }
+    // if (!user) throw new UnauthorizedException();
+    
     return user;
   }
 }
