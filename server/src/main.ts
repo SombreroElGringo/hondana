d590b0efd508bdfd17c7122b63364273fcbdfe49
SombreroElGringo <florent.pailhes@hotmail.fr>
2018-12-06 10:39:05 +0100

    Enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,6 +8,7 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
+  app.enableCors();
   app.connectMicroservice({
     transport: Transport.TCP,
     options: { retryAttempts: 5, retryDelay: 3000 },
