8718ba307d9e972e767e97b1de7635e1c89fd430
Maxime Boisse <maxime.boisse@protonmail.com>
2018-12-08 02:13:55 +0100

    feature/HD-20: finish userRating component
    finish the component to display the rate of the current user. need to be update after the db conf

--- client/src/components/UserRating/UserRating.js
+++ client/src/components/UserRating/UserRating.js
@@ -7,29 +7,44 @@ class UserRating extends Component{
         rightComma: 0
     }
 
+    splitRating(){
+
+    }
+    
     componentDidMount(){
-        const {userRating} = this.props;
-        const {leftComma, rightComma} = this.state;
+        // const {userRating} = this.props;
+        const userRating = Number.parseFloat(3).toFixed(1);
 
-        const ratingRender = [];
-
-        if(userRating){
-            const sUserRating = userRating.toString();
-            const ratingArray = sUserRating.split('.')
-            this.setState(
-                {
-                    leftComma: parseInt(ratingArray[0]), 
-                    rightComma: parseInt(ratingArray[1])
-                }
-            )
-        }
-    console.log(leftComma, rightComma)
+        const sUserRating = userRating.toString();
+        const ratingArray = sUserRating.split('.')
+        this.setState(
+            {
+                leftComma: parseInt(ratingArray[0]), 
+                rightComma: parseInt(ratingArray[1])
+            }
+        )
 }
 
     render(){
+        const {leftComma, rightComma} = this.state;
+        let ratingRender = [];
+
+        for (let i = 0; i < leftComma; i++) {
+            ratingRender.push(<span className="fa fa-star checked" key={i}></span>)
+        }
+        if(rightComma > 2 && rightComma <= 7)
+            ratingRender.push(<span className="fa fa-star-half checked"></span>)
+        else if(rightComma > 7)
+            ratingRender.push(<span className="fa fa-star checked"></span>)  
+        else if(rightComma <= 2)
+            ratingRender.push(<span className="fa fa-star"></span>)
+        if(ratingRender.length<5)
+            for (let i = ratingRender.length; i < 5; i++) {
+                ratingRender.push(<span className="fa fa-star"></span>)               
+            }
         return(
-            <div>
-                <p>MAchin</p>
+            <div className='user--rating'>
+                {ratingRender}
             </div>
         )
     }
