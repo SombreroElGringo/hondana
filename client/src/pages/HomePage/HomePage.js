db3250049215140faaa8c0c3f5485d1359ea78ee
Dylan Chamoulaud <dylan3396@hotmail.fr>
2018-12-13 15:50:29 +0100

    feature(HD-40): Book page improvement
    - Coordinates management on leaflet map
    - Access to bookcase details from books list

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import './HomePage.css';
-import { connect } from 'react-redux';
-import { fetchBooks } from '../../redux/actions/app';
-import getBooks from '../../redux/selectors/app/getBooks';
 import SearchBook from '../../components/SearchBook/SearchBook';
-import { mapDispatchToProps, mapStateToProps } from '../../utils/redux_helpers';
 
 class HomePage extends Component {
   render() {
-    const { books, fetchBooks, history } = this.props;
+    const { history } = this.props;
     return (
       <div className="container" id="homepage">
         <img
@@ -16,17 +12,10 @@ class HomePage extends Component {
           src="http://mojehobbi.pl/wp-content/uploads/2015/12/leaves-1076307_1280.jpg"
           alt=""
         />
-        <SearchBook fetchBooks={fetchBooks} books={books} history={history} />
+        <SearchBook history={history} />
       </div>
     );
   }
 }
 
-export default connect(
-  mapStateToProps({
-    books: getBooks,
-  }),
-  mapDispatchToProps({
-    fetchBooks,
-  })
-)(HomePage);
+export default HomePage;
