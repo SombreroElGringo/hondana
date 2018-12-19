bb195ba2baa6017a63e4971974eb4137f6142259
Dylan Chamoulaud <dylan3396@hotmail.fr>
2018-12-19 20:55:08 +0100

    fix concat

--- client/src/utils/cookie_helpers.js
+++ client/src/utils/cookie_helpers.js
@@ -2,11 +2,11 @@ export function setCookie(cname, cvalue, exdays) {
   const d = new Date(); //Create an date object
   d.setTime(d.getTime() + exdays * 1000 * 60 * 60 * 24); //Set the time to exdays from the current date in milliseconds. 1000 milliseonds = 1 second
   const expires = 'expires=' + d.toGMTString(); //Compose the expirartion date
-  window.document.cookie = cname + '=' + cvalue + '; ' + expires; //Set the cookie with value and the expiration date
+  window.document.cookie = `${cname}=${cvalue}; ${expires}`; //Set the cookie with value and the expiration date
 }
 
 export function getCookie(cname) {
-  const name = cname + '='; //Create the cookie name variable with cookie name concatenate with = sign
+  const name = `${cname}=`; //Create the cookie name variable with cookie name concatenate with = sign
   const cArr = window.document.cookie.split(';'); //Create cookie array by split the cookie by ';'
 
   //Loop through the cookies and return the cooki value if it find the cookie name
@@ -23,6 +23,6 @@ export function getCookie(cname) {
 export function deleteCookie(cname) {
   const d = new Date(); //Create an date object
   d.setTime(d.getTime() - 1000 * 60 * 60 * 24); //Set the time to the past. 1000 milliseonds = 1 second
-  const expires = 'expires=' + d.toGMTString(); //Compose the expirartion date
-  window.document.cookie = cname + '=' + '; ' + expires; //Set the cookie with name and the expiration date
+  const expires = `expires=${d.toGMTString()}`; //Compose the expirartion date
+  window.document.cookie = `${cname}=; ${expires}`; //Set the cookie with name and the expiration date
 }
